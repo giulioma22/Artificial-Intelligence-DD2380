9b53a0e846794b47da34ce773095393d12309149
giulioma22 <giulioma@kth.se>
2019-03-06 17:07:40 +0100

    Created folders

--- A1/Duck_Hunt/duckhunt_skeleton_java/HMM3.java
+++ A1/Duck_Hunt/duckhunt_skeleton_java/HMM3.java
@@ -14,10 +14,71 @@ public class HMM3{
 
     int N, M, T, nRows;
     double multiplier = 0.2;
+    long seed = (long)5000000000.0;
     double[] info;
 
 
     // - - - - - - DUCK HUNT IMPLEMENTATIONS - - - - - -
+
+
+    public HMM3(int N, int M, int nRows, int T, int[] obs_sequence){
+        random = new Random(seed);
+
+        this.N = N;
+        this.M = M;
+        this.nRows = nRows;
+        this.transition = new double[this.N][this.N];
+        this.emission = new double[this.N][this.M];
+        this.initial_prob = new double[this.nRows][this.N];
+
+        //A Matrix - Transition
+
+        for (int i = 0; i < this.N; i++) {
+            double holder = 0;
+            for (int t = 0; t < this.N; t++) {
+                this.transition[i][t] = 1 / this.N + random.nextDouble() * multiplier / this.N;
+                holder += this.transition[i][t];
+            }
+            for (int j = 0; j < this.N; j++) {
+                this.transition[i][j] = this.transition[i][j] / holder;
+            }
+        }
+
+        //B Matrix - Emission
+
+        for (int i = 0; i < this.N; i++) {
+            double holder = 0;
+            for (int t = 0; t < this.M; t++) {
+                this.emission[i][t] = 1 / this.M + random.nextDouble() * multiplier / this.M;
+                holder += this.emission[i][t];
+            }
+            for (int j = 0; j < this.M; j++){
+                this.emission[i][j] = this.emission[i][j] / holder;
+            }
+
+        }
+
+        //Initial probability
+
+        double holder = 0;
+        for (int i = 0; i < this.N; i++) {
+            initial_prob[0][i] = 1 / this.N + random.nextDouble() * multiplier / this.N;
+            holder += initial_prob[0][i];
+        }
+        for (int i = 0; i < this.N; i++){
+            initial_prob[0][i] =  initial_prob[0][i] /holder;
+        }
+
+        //Observation sequence
+
+        this.T = T;
+        this.obs_sequence = obs_sequence;
+
+
+    }
+
+
+    // - - - - - - - - - - - - - - - - - - -
 
 
     public void HMM_algorithm(){
@@ -239,66 +300,6 @@ public class HMM3{
     // - - - - - - - - - - - - - - - - - - -
 
 
-    public HMM3(int N, int M, int nRows, int T, int[] obs_sequence){
-        random = new Random();
-
-        this.N = N;
-        this.M = M;
-        this.nRows = nRows;
-        this.transition = new double[this.N][this.N];
-        this.emission = new double[this.N][this.M];
-        this.initial_prob = new double[this.nRows][this.N];
-
-        //A Matrix
-
-        for (int i = 0; i < this.N; i++) {
-            double holder = 0;
-            for (int t = 0; t < this.N; t++) {
-                this.transition[i][t] = 1 / this.N + random.nextDouble() * multiplier / this.N;
-                holder += this.transition[i][t];
-            }
-            for (int j = 0; j < this.N; j++) {
-                this.transition[i][j] /= holder;
-            }
-        }
-
-        //B Matrix
-
-        for (int i = 0; i < this.N; i++) {
-            double holder = 0;
-            for (int t = 0; t < this.M; t++) {
-                this.emission[i][t] = 1 / this.M + random.nextDouble() * multiplier / this.M;
-                holder += this.emission[i][t];
-            }
-            for (int j = 0; j < this.M; j++){
-                this.emission[i][j] /= holder;
-            }
-
-        }
-
-        //Initial probability
-
-        double holder = 0;
-        for (int i = 0; i < this.N; i++) {
-            initial_prob[0][i] = 1 / this.N + random.nextDouble() * multiplier / this.N;
-            holder += initial_prob[0][i];
-        }
-        for (int i = 0; i < this.N; i++){
-        initial_prob[0][i] /=  holder;
-        }
-
-        //Observation sequence
-
-        this.T = T;
-        this.obs_sequence = obs_sequence;
-
-
-    }
-
-
-    // - - - - - - - - - - - - - - - - - - -
-
-
     public double updateAlpha(int[] observations){
         double prob = 0;
         double[][] alpha = new double[observations.length][this.N];
@@ -353,54 +354,6 @@ public class HMM3{
             }
         }
         return maxIdx;
-    }
-
-
-    // - - - - - - FUNCTIONS - - - - - -
-
-
-    public static void printmatrix2string(double mat[][])
-    {
-        double[][] matrix =  mat;
-        int row = matrix.length;
-        int col = matrix[0].length;
-
-        System.out.print(row + " " + col + " ");
-        for (int i = 0; i < mat.length; i++) {
-
-            // Loop through all elements of current row
-            for (int j = 0; j < mat[i].length; j++)
-                System.out.print(mat[i][j] + " ");
-        }
-        return;
-    }
-
-    public static void print2D(double mat[][])
-    {
-        // Loop through all rows
-        for (int i = 0; i < mat.length; i++) {
-
-            // Loop through all elements of current row
-            for (int j = 0; j < mat[0].length; j++)
-                System.out.print(mat[i][j] + " ");
-
-            System.out.println();
-        }
-    }
-
-    public static double[][] matrixMulti(double mat1[][], double mat2[][]) {
-
-        double[][] product = new double[mat1.length][mat2[0].length];
-
-        for (int i = 0; i < mat1.length; i++) {
-            for (int j = 0; j < mat2[0].length; j++) {
-                for (int k = 0; k < mat1[0].length; k++) {
-                    product[i][j] += mat1[i][k] * mat2[k][j];
-                }
-            }
-        }
-
-        return product;
     }
 
 }
